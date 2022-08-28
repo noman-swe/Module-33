a8a91c41843f981ffb985f53bea9c94ad513927a
noman-swe <noman.swe130@gmail.com>
2022-08-29 01:06:07 +0600

    33-4 Load more data, more APIs, send data to function

--- API-Introduction/js/api.js
+++ API-Introduction/js/api.js
@@ -1,5 +1,27 @@
-function loadData(){
+function loadData() {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(data => console.log(data.title))
+        .then(response => response.json())
+        .then(data => console.log(data.title))
+}
+
+// 
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => displayUsers(data));
+}
+
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+// 
+function displayUsers(data){
+    for(const SoloData of data){
+        console.log(SoloData.name);
+    }
+    /* const soloData = data.map(user => user.name);//map() creates a new array from calling a function for every array element.
+    console.log(soloData);  */
 }
