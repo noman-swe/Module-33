4d3bcc433f66c5a0bff773e71feb25de2dbcf046
noman-swe <noman.swe130@gmail.com>
2022-08-28 23:24:16 +0600

    33-2 Intro to JSON, JSON Structure, parse, stringify

--- API-Introduction/js/index.js
+++ API-Introduction/js/index.js
@@ -1,2 +1,23 @@
 const user = { id:01, name: 'Gorib Amir',  job: 'Actor' };
-console.log(user);
+const stringified = JSON.stringify(user);
+// console.log(stringified);
+
+const shop = {
+    name: 'Alia Store',
+    address: 'Ranbir Road',
+    profit: 1500,
+    products: ['laptop', 'pepsi', 'chips'],
+    owner: {
+        name:'Alia Vaat', profession: 'Actor',
+    },
+    isExpensive: false,
+}
+
+const shopStringified = JSON.stringify(shop);
+console.log(shopStringified);
+// console.log(shop);
+
+// convert a stringify
+const converted = JSON.parse(shopStringified);
+console.log(converted);
+console.log(converted.owner);
