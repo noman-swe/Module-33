3971d801b74af2d1f19d9587045963a5cbed4c02
noman-swe <noman.swe130@gmail.com>
2022-08-29 09:59:06 +0600

    33-7 GET, POST, PATCH, DELETE, CRUD, GET Vs POST

--- API-Introduction/Posts/js/posts.js
+++ API-Introduction/Posts/js/posts.js
@@ -12,3 +12,20 @@ function displayPosts(posts) {
         postSection.appendChild(article);
     }
 }
+
+
+// 
+
+function addAPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'My New Posts',
+            body: 'This is my posts',
+            userId : 1
+        }),
+        headers: {  'Content-type': 'application/json; charset=UTF-8', },
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
